5300a1e83f7befadd1d4b34a2285e7ff5870f214
akakou <akakou571@gmail.com>
2022-06-03 05:32:06 +0900

    [remove] unused code

--- components/hello.tsx
+++
@@ -1,6 +0,0 @@
-import { setup } from "../libs/sketch"
-
-export function Hello() {
-    setup()
-    return <p>Hello!</p>
-}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Button } from "@chakra-ui/react"
-import dynamic from 'next/dynamic'
-import { useEffect } from 'react'
-import Script from 'next/script'
 
 
 const Home = () => {

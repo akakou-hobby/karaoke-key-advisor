abe5c972306e303653ed04529aecd9bca73fa027
akakou <akakou571@gmail.com>
2022-06-04 17:51:02 +0900

    [update] base ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import { ChakraProvider } from "@chakra-ui/react"
 import Script from 'next/script'
 function MyApp({ Component, pageProps }: AppProps) {
   return <ChakraProvider>
+    <link href="https://fonts.googleapis.com/css?family=M+PLUS+1p" rel="stylesheet"></link>
     <Component {...pageProps} />
     <Script src="https://api.songle.jp/v2/api.js" strategy="beforeInteractive"></Script>
   </ChakraProvider>

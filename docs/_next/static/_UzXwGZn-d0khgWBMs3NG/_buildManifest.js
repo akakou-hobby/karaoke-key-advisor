4387493c3201f016fb0735d25f2fd5eb9f862786
akakou <akakou571@gmail.com>
2022-06-05 03:39:51 +0900

    [update] docs => out

---
+++ docs/_next/static/_UzXwGZn-d0khgWBMs3NG/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/563-883a04ebf173af45.js","static/chunks/pages/index-4c0fb076a17d6294.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

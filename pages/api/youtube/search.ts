b53ed9ee87b9c1dae80fd668755abe2c0f32150e
kino-ma <ma@kino.ma>
2022-09-18 00:15:45 +0900

    refactor: add types for api results

--- pages/api/youtube/search.ts
+++ pages/api/youtube/search.ts
@@ -8,6 +8,28 @@ enum HTTP_STATUS {
   METHOD_NOT_ALLOWED = 405,
 }
 
+type ApiResust = {
+  items: {
+    id: {
+      videoId: string;
+    };
+    snippet: {
+      title: string;
+      thumbnails: {
+        [key: string /*"default" | "medium" | "high"*/]: {
+          url: string;
+          width: number;
+          height: number;
+        };
+      };
+    };
+  }[];
+};
+
+interface SearchResult extends ApiResust {}
+
+interface YouTubeSearchResponse {}
+
 export default async function search(
   req: NextApiRequest,
   res: NextApiResponse
@@ -40,7 +62,9 @@ export default async function search(
     videoCategoryId,
   };
 
-  const { data } = await axios.get(SEARCH_ENDPOINT, { params });
+  const { data } = await axios.get<YouTubeSearchResponse>(SEARCH_ENDPOINT, {
+    params,
+  });
 
-  res.status(200).json(data);
+  res.status(200).json(data as ApiResust);
 }

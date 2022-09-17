0ef74aabf0421c16c2d72e81043cbf89d3312c02
kino-ma <ma@kino.ma>
2022-09-17 23:00:14 +0900

    refactor: reject non-POST requests

--- pages/api/youtube/search.ts
+++ pages/api/youtube/search.ts
@@ -1,12 +1,22 @@
 import axios from "axios";
+import { METHODS } from "http";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const SEARCH_ENDPOINT = "https://www.googleapis.com/youtube/v3/search";
+
+enum HTTP_STATUS {
+  METHOD_NOT_ALLOWED = 405,
+}
 
 export default async function search(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  if (req.method !== "GET") {
+    res.status(HTTP_STATUS.METHOD_NOT_ALLOWED);
+    return;
+  }
+
   const key = process.env.YOUTUBE_API_KEY;
   const type = "video";
   const q = "hoge";
